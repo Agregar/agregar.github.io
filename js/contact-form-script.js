9f7ac102d22fb7ec4fd1d24053915c8b10d6ee6e
Owen Craig <owen@owencraig.com>
2017-07-25 21:57:10 +1000

    Re-adds contact form

--- js/contact-form-script.js
+++ js/contact-form-script.js
@@ -11,7 +11,7 @@ $("#contactForm").validator().on("submit", function (event) {
 });
 
 
-function submitForm(){
+function submitForm() {
     // Initiate Variables With Form Content
     var name = $("#name").val();
     var email = $("#email").val();
@@ -21,32 +21,33 @@ function submitForm(){
 
     $.ajax({
         type: "POST",
-        url: "php/form-process.php",
-        data: "name=" + name + "&email=" + email + "&msg_subject=" + msg_subject + "&message=" + message,
-        success : function(text){
-            if (text == "success"){
-                formSuccess();
-            } else {
-                formError();
-                submitMSG(false,text);
-            }
+        url: "https://prod-18.australiasoutheast.logic.azure.com:443/workflows/5ce213cf65654eaab7e917e42dd04ab0/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=TsgSV3atFiFIlAmHuGhNW2c1Ox0Y_jwyhInkudgsUtk",
+        contentType: "application/json",
+        data: JSON.stringify({ name: name, email: email, msg_subject: msg_subject, message: message }),
+        dataType: "text",
+        success: function (text) {
+            formSuccess();
+        },
+        error: function (err) {
+            formError();
+            submitMSG(false, text);
         }
     });
 }
 
-function formSuccess(){
+function formSuccess() {
     $("#contactForm")[0].reset();
     submitMSG(true, "Message Submitted!")
 }
 
-function formError(){
-    $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+function formError() {
+    $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
         $(this).removeClass();
     });
 }
 
-function submitMSG(valid, msg){
-    if(valid){
+function submitMSG(valid, msg) {
+    if (valid) {
         var msgClasses = "h3 text-center tada animated text-success";
     } else {
         var msgClasses = "h3 text-center text-danger";
